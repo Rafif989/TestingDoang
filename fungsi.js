f5366c6eadc2b2b9c774535330ba8b6f10292257
Rafif Ulwannafis <raulrafifulwannafis@gmail.com>
2023-06-16 17:26:24 +0700

    menambahkan fungdi bagi

--- fungsi.js
+++ fungsi.js
@@ -5,4 +5,7 @@ const subtarct = (a,b) => a-b;
 console.log(subtarct(5,2)); 
 
 const multiply=(a,b) => a * b;
-console.log(multiply(2,2))
+console.log(multiply(2,2));
+
+const devide= (a,b) => a / b;
+console.log (devide(4,2));
